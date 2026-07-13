e3a144ed9196eac12fb96acc0edc24db03cdb181
niloy903283-rgb <niloy903283@gmail.com>
2026-07-13 12:51:33 +0600

    Update game.js

--- game.js
+++ game.js
@@ -1,11 +1,7 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-const road = {
-  offset: 0,
-  speed: 6
-};
-
+// Player
 const player = {
   x: 155,
   y: 520,
@@ -14,61 +10,66 @@ const player = {
   speed: 7
 };
 
+// Enemy
+let enemy = {
+  x: Math.random() * 240 + 40,
+  y: -120,
+  width: 50,
+  height: 90,
+  speed: 6
+};
+
+let roadOffset = 0;
+let score = 0;
+let gameOver = false;
+
 const keys = {};
 
-document.addEventListener("keydown", (e) => {
+document.addEventListener("keydown", e => {
   keys[e.key] = true;
 });
 
-document.addEventListener("keyup", (e) => {
+document.addEventListener("keyup", e => {
   keys[e.key] = false;
 });
 
-function update() {
+function update(){
 
-  if (keys["ArrowLeft"]) {
-    player.x -= player.speed;
+  if(gameOver) return;
+
+  if(keys["ArrowLeft"]) player.x -= player.speed;
+  if(keys["ArrowRight"]) player.x += player.speed;
+
+  if(player.x < 20) player.x = 20;
+  if(player.x > 290) player.x = 290;
+
+  roadOffset += 6;
+  if(roadOffset >= 80) roadOffset = 0;
+
+  enemy.y += enemy.speed;
+
+  if(enemy.y > 700){
+      enemy.y = -120;
+      enemy.x = Math.random()*240+40;
+      score++;
   }
 
-  if (keys["ArrowRight"]) {
-    player.x += player.speed;
-  }
-
-  if (player.x < 20) player.x = 20;
-  if (player.x > 290) player.x = 290;
-
-  road.offset += road.speed;
-  if (road.offset >= 80) road.offset = 0;
-}
-
-function drawRoad() {
-
-  ctx.fillStyle = "#555";
-  ctx.fillRect(0, 0, 360, 640);
-
-  ctx.fillStyle = "#ffffff";
-
-  for (let y = -80; y < 640; y += 80) {
-    ctx.fillRect(175, y + road.offset, 10, 40);
+  // Collision
+  if(
+      player.x < enemy.x + enemy.width &&
+      player.x + player.width > enemy.x &&
+      player.y < enemy.y + enemy.height &&
+      player.y + player.height > enemy.y
+  ){
+      gameOver = true;
   }
 }
 
-function drawPlayer() {
+function drawRoad(){
 
-  ctx.fillStyle = "#ff0000";
-  ctx.fillRect(player.x, player.y, player.width, player.height);
-}
+  ctx.fillStyle="#555";
+  ctx.fillRect(0,0,360,640);
 
-function gameLoop() {
+  ctx.fillStyle="white";
 
-  update();
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  drawRoad();
-  drawPlayer();
-
-  requestAnimationFrame(gameLoop);
-}
-
-gameLoop();
+  for(let i=-
